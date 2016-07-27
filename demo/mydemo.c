791a635cbc34593052c9bd9079ea529932c6514f
Maximilian PL Haslbeck <haslbema@in.tum.de>
2016-07-27 09:27:35 +0200

    added the example task of the first session

---
+++ demo/mydemo.c
@@ -0,0 +1,30 @@
+
+
+
+int f(int x) {
+  if(x==4)
+      return 2;
+  else
+      return 0;
+}
+
+int g(int x) {
+  while(x!=0) { 
+    if(x>0) x = x-1;
+    else  x = x+1;
+  }
+  return 0;
+}
+
+/*
+int h(int x) {
+  if(x==0) {
+    return 0;
+  } else if(x>0) {
+    return h (x-1);
+  } else {
+    return h (x+1);
+  }
+}
+*/
+
